cffb9d95ecc6af82f909ef2e847d19b5747902ad
crtmachiny <crtmachiny@tf.otbebucb>
2026-03-06 17:27:52 +0300

    Добавлен класс локаторов и Page Object страницы авторизации и входа

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,16 @@
 from selenium.webdriver import Chrome
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage:
     def __init__(self, browser, url: str, timeout: int = 0):
         self.browser = browser
         self.url = url
         self.timeout = timeout
-    
+
     def open(self):
         self.browser.get(self.url)
-        
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,20 @@
 from selenium.webdriver.common.by import By
 from .pages import MainPage
 
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = LINK
     page = MainPage(browser, link)
-    
+
     page.open()
     page.go_to_login_page()
-    
-    
+
+
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = LINK
     page = MainPage(browser, link)
-    
+
     page.open()
     page.should_be_login_link()
